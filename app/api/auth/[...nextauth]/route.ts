c88316e0da836e03a75b393b17d270b69a427fcf
Tiago <tgspn@users.noreply.github.com>
2025-11-05 17:24:47 -0300

    fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,3 @@
 // import { handlers } from "@/lib/auth"
 
 // export const { GET, POST } = handlers
-
-import { handlers } from "@/lib/auth"
-
-// (Opcional, mas ajuda com libs OIDC): 
-export const runtime = "nodejs"
-
-export const { GET, POST } = handlers
